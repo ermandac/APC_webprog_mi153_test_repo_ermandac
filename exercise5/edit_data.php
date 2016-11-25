2a97202125888da735ef65afef83ffc3eeab7c78
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2016-11-25 13:24:22 +0800

    edited edit_data.php

--- exercise5/edit_data.php
+++ exercise5/edit_data.php
@@ -12,10 +12,12 @@ if(isset($_POST['btn-update']))
  $first_name = $_POST['first_name'];
  $last_name = $_POST['last_name'];
  $city_name = $_POST['city_name'];
+ $contact_no = $_POST['contact_no'];
+ $email = $_POST['email'];
  // variables for input data
 
  // sql query for update data into database
- $sql_query = "UPDATE users SET first_name='$first_name',last_name='$last_name',user_city='$city_name' WHERE user_id=".$_GET['edit_id'];
+ $sql_query = "UPDATE users SET first_name='$first_name',last_name='$last_name',user_city='$city_name',contact_no='$contact_no',email='$email' WHERE user_id=".$_GET['edit_id'];
  // sql query for update data into database
  
  // sql query execution function
@@ -70,7 +72,11 @@ if(isset($_POST['btn-cancel']))
     <td><input type="text" name="last_name" placeholder="Last Name" value="<?php echo $fetched_row['last_name']; ?>" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="city_name" placeholder="City" value="<?php echo $fetched_row['user_city']; ?>" required /></td>
+    <td><input type="text" name="city_name" placeholder="City" value="<?php echo $fetched_row['city_name']; ?>" required /></td>
+    </tr>
+	<td><input type="text" name="contact_no" placeholder="Contact Number" value="<?php echo $fetched_row['contact_no']; ?>" required /></td>
+    </tr>
+	<td><input type="text" name="email" placeholder="Email" value="<?php echo $fetched_row['email']; ?>" required /></td>
     </tr>
     <tr>
     <td>
