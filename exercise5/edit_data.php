dec82af4faf2f3a7489f46ee3ab0f021fddc3d76
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2016-12-13 12:18:27 +0800

    edited edit_data.php

--- exercise5/edit_data.php
+++ exercise5/edit_data.php
@@ -2,7 +2,7 @@
 include_once 'dbconfig.php';
 if(isset($_GET['edit_id']))
 {
- $sql_query="SELECT * FROM users WHERE user_id=".$_GET['edt_id'];
+ $sql_query="SELECT * FROM users WHERE user_id=".$_GET['edit_id'];
  $result_set=mysqli_query($con, $sql_query);
  $fetched_row=mysqli_fetch_array($result_set);
 }
