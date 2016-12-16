02b4ecf16c665c50fbc8945e79c99dfbb78d04f6
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2016-12-16 12:30:26 +0800

    edited dbconfig.php

--- exercise5/dbconfig.php
+++ exercise5/dbconfig.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$datbase = "guestdb";
+$datbase = "dbtuts";
 $con = mysqli_connect($host,$user,$password);
 mysqli_select_db($con, $datbase);
 ?>
